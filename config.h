5f5d3fd53a55417e4b6a8bdfd5413ff0621a2719
Sreevatsank Kadaveru <skadaveru@ucsd.edu>
2022-05-11 15:33:43 -0700

    Added __aarch64 flag to config to enable compile on ARM64 chips

--- config.h
+++ config.h
@@ -56,7 +56,7 @@
 #endif
 
 
-#if (__i386 || __amd64 || __powerpc__) && __GNUC__
+#if (__i386 || __amd64 || __powerpc__ || __aarch64__) && __GNUC__
 #define GNUC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #if (defined(__GLIBC__) && defined(__GLIBC_PREREQ))
 #if (GNUC_VERSION >= 40100 && __GLIBC_PREREQ(2, 6))
